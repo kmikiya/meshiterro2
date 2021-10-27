a5b02a38b816e5c73e13208e87bfea5adb98792d
kmikiya <k.mikiya97@gmail.com>
2021-10-27 13:37:02 +0000

    飯テロ

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'b2a782c8d865284d78181e3c3b16e7c891864d13dcb6c76686abfcd939f1e7a934a56174c1a8f682819ad743e2ea64345342bfdc709e9103b2116d7c1d0c3dbb'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  resources :post_images do
+    resource :favorites, only:[:create, :destroy]
+  end
+  resources :users
+end
